28bd42437a09a9cc6c2eb08d3dba9b7c8e7f1da9
alexandre-piron <alexandre.piron01@etu.umontpellier.fr>
2024-01-22 10:34:47 +0100

    maj des tables

--- database/migrations/2024_01_22_092930_create_type_user_table.php
+++ database/migrations/2024_01_22_092930_create_type_user_table.php
@@ -11,15 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('type_user', function (Blueprint $table) {
             $table->id();
             $table->string('nom');
-            $table->string('prenom');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->integer('age');
-            $table->rememberToken();
             $table->timestamps();
         });
     }
@@ -29,6 +23,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('type_user');
     }
 };
